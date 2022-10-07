78c8f57bdbff7501964cc955a0c510c0d477c7a7
Gurpreet Singh <64520145+gssandhu143@users.noreply.github.com>
2022-10-07 13:04:21 +0100

    Update book.java

--- src/main/java/servlet/book.java
+++ src/main/java/servlet/book.java
@@ -31,12 +31,9 @@ public class book extends HttpServlet {
 
     public void init() {
         // Get the file location where it would be stored.
-        db
-                = getServletContext().getInitParameter("mysql-db");
-        dbuser
-                = getServletContext().getInitParameter("mysql-user");
-        dbpass
-                = getServletContext().getInitParameter("mysql-pass");
+        db = getServletContext().getInitParameter("mysql-db");
+        dbuser = getServletContext().getInitParameter("mysql-user");
+        dbpass = getServletContext().getInitParameter("mysql-pass");
     }
 
     /**
@@ -49,7 +46,7 @@ public class book extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         HttpSession session = request.getSession();
@@ -67,9 +64,9 @@ public class book extends HttpServlet {
             Connection connection = DriverManager.getConnection("jdbc:mysql:///" + db + "", dbuser, dbpass);
             CallableStatement cstmt = null;
             Statement stmt = connection.createStatement();
-            String SQL = "{call sp_bookflights"
-                    + "(" + flightid + "," + userid + ",'" + date + "',"
-                    + "'" + time + "')}";
+            String SQL = "{call sp_bookflights" +
+                "(" + flightid + "," + userid + ",'" + date + "'," +
+                "'" + time + "')}";
 
             cstmt = connection.prepareCall(SQL);
             ResultSet rs = cstmt.executeQuery(SQL);
@@ -80,14 +77,14 @@ public class book extends HttpServlet {
                     if ("bid".equals(rsmd.getColumnName(x))) {
                         session.setAttribute("bid", rs.getString("bid"));
                         int bid = rs.getInt("bid");
-                        ResultSet rs2 = stmt.executeQuery("select f.flightnumber,f.source,f.destination,b.flightdate,b.flighttime,u.email from "
-                                + "flightmaster f join bookingmaster b on f.flightid = b.flightid join usermaster u on u.userid = b.userid where b.id = " + bid + "");
+                        ResultSet rs2 = stmt.executeQuery("select f.flightnumber,f.source,f.destination,b.flightdate,b.flighttime,u.email from " +
+                            "flightmaster f join bookingmaster b on f.flightid = b.flightid join usermaster u on u.userid = b.userid where b.id = " + bid + "");
                         while (rs2.next()) {
                             String sub = "Your flight details.";
-                            String msg = "Thanks for book Flight."
-                                    + "<br>" + "Your flight details " + "<br>" + ""
-                                    + "Flight number : " + rs2.getString("flightnumber") + "" + "<br>" + "Source :" + rs2.getString("source") + "" + "<br>"
-                                    + "Destination : " + rs2.getString("destination") + "" + "<br>" + "Flight date : " + rs2.getString("flightdate") + "" + "<br>" + "Flight time : " + rs2.getString("flighttime") + "";
+                            String msg = "Thanks for book Flight." +
+                                "<br>" + "Your flight details " + "<br>" + "" +
+                                "Flight number : " + rs2.getString("flightnumber") + "" + "<br>" + "Source :" + rs2.getString("source") + "" + "<br>" +
+                                "Destination : " + rs2.getString("destination") + "" + "<br>" + "Flight date : " + rs2.getString("flightdate") + "" + "<br>" + "Flight time : " + rs2.getString("flighttime") + "";
                             emailsender.send(rs2.getString("email"), sub, msg);
                         }
 
@@ -112,7 +109,7 @@ public class book extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -126,7 +123,7 @@ public class book extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -138,6 +135,6 @@ public class book extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    } // </editor-fold>
 
 }
